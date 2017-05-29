19c45643240561c71275c972e424ab7f8e4922dd
T.J. Hartman <tjhartman@TJs-MacBook-Pro.local>
2017-05-29 14:46:59 -0600

    nearly there

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -21,6 +21,14 @@ angular.module('routesApp', ['ui.router']).config(function ($stateProvider, $url
 "use strict";
 'use strict';
 
+angular.module('routesApp').service('mainSvc', function ($http) {
+
+    this.getRoutes = function () {
+        return $http.get('https://www.mountainproject.com/data?action=getRoutes&routeIds=108184604,108955192,108955188,110374423,107736171,109055762,106822406,106645813,107102098,107102520&key=200069508-bf86aa531f568aff3a52cb57d1fd452d');
+    };
+});
+'use strict';
+
 angular.module('routesApp').controller('mainCtrl', function ($scope, mainSvc) {
 
     $scope.recRoutes = function () {
@@ -36,13 +44,5 @@ angular.module('routesApp').controller('mainCtrl', function ($scope, mainSvc) {
     // $scope.typeInProcess= function() {      
     //   $scope.typeInProcess = true;
     // };
-});
-'use strict';
-
-angular.module('routesApp').service('mainSvc', function ($http) {
-
-    this.getRoutes = function () {
-        return $http.get('https://www.mountainproject.com/data?action=getRoutes&routeIds=108955192,108955188,110374423,107736171,109055762,106822406,106645813,107102098,107102520&key=200069508-bf86aa531f568aff3a52cb57d1fd452d');
-    };
 });
 //# sourceMappingURL=bundle.js.map
